3f500d33ed71fbed5c95f9eb9dcea06e945af861
T357A <Sean@CurrentElectrical.Services>
2021-05-01 19:39:30 -0400

    Master Layout

--- TBS.py
+++ TBS.py
@@ -1,22 +1,31 @@
-''' Create dog walking app '''
+""" Create dog walking app """
 # connect to database (SQLITE that syncs with online data)
 
-#API's- weather (temperature), maps to track routes, find lost last location
+# API's- weather (temperature), maps to track routes, find lost last location
 
+
+# ***Weather***
+# Heat stroke, frostbite, dehydration risks (Chart?)
+# emergency weather alerts
 
 # ***Counter***
 # for daily logs for walks- to reset at midnight
 # Stats would show daily, weekly,monthly and annual totals to date.
 
-# ***Mapping*** 
+# ***Mapping***
 # for fitness goals --- See Stats
 #  Also for warnings of dangerous areas or approaching/ unfriendly/ non social pets
 # Live tracking/ share with firends/ family
 # best/ new sniffing areas
 
 # ***Pet Profile:***
+# Photos
+# Vetranairian
+# Medicines, vacines
+# Alergies
 # name , owners name, cell # (possibly hidden and send message through app?)
-# Age, Breed, sex, eye colour, colour(s), other(birthmarks)
+# Age, Breed, sex, eye colour, colour(s), other(birthmarks), weight
+# Diet (brands), treats amounts
 # Emergency contact
 #  Emergency alert broadcast for given map area.
 
@@ -24,17 +33,24 @@
 # Distance walked
 # Most often sniffed
 # Most common area sniffed
-
+# Challenges and results
+# Manual adding?
+# Health issues ( runs, sickness, tiredness)
 
 # ***Hardware *** (Colllar)
 # If it is a leash, it could collect heart rate and pets temperature ofr duress situations.
 # Bluetooth for nearby and "herd" alerts if there are other members nearby with their app open, they could be alerted for lost pet etc.
 
-
 # ***Hardware Part 2***(Tag)
 # local air temp
 
 # ***Notifications and reminders***
-#Temperature  = Hydration, frostbite
+# Temperature  = Hydration, frostbite
 # Duress body temperature warnings
 # Weather warnings = geolocation
+
+# ***Apple watch/ other wearable***
+# T.P.E total pets earned- by the pets furless money NFC would log if watch is within 1 foot and making petting motion to earn points for the furrless monkey.
+
+
+# Use the BLIND data to sell to industry related product manufacturers (dog food)- keeps the app cheaper to use.
