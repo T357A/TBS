bc25654b363727ce70a982cd9a538061e7155476
T357A <Sean@CurrentElectrical.Services>
2021-05-01 19:19:23 -0400

    Still commenting app features

--- TBS.py
+++ TBS.py
@@ -1,1 +1,40 @@
 ''' Create dog walking app '''
+# connect to database (SQLITE that syncs with online data)
+
+#API's- weather (temperature), maps to track routes, find lost last location
+
+
+# ***Counter***
+# for daily logs for walks- to reset at midnight
+# Stats would show daily, weekly,monthly and annual totals to date.
+
+# ***Mapping*** 
+# for fitness goals --- See Stats
+#  Also for warnings of dangerous areas or approaching/ unfriendly/ non social pets
+# Live tracking/ share with firends/ family
+# best/ new sniffing areas
+
+# ***Pet Profile:***
+# name , owners name, cell # (possibly hidden and send message through app?)
+# Age, Breed, sex, eye colour, colour(s), other(birthmarks)
+# Emergency contact
+#  Emergency alert broadcast for given map area.
+
+# ***Stats***
+# Distance walked
+# Most often sniffed
+# Most common area sniffed
+
+
+# ***Hardware *** (Colllar)
+# If it is a leash, it could collect heart rate and pets temperature ofr duress situations.
+# Bluetooth for nearby and "herd" alerts if there are other members nearby with their app open, they could be alerted for lost pet etc.
+
+
+# ***Hardware Part 2***(Tag)
+# local air temp
+
+# ***Notifications and reminders***
+#Temperature  = Hydration, frostbite
+# Duress body temperature warnings
+# Weather warnings = geolocation
